1c675700855e7c38ce86c7071eb84431c17a0a07
ivan <ivanlp10n2@gmail.com>
2022-03-07 13:02:42 -0300

    refactoring

--- build.sbt
+++ build.sbt
@@ -5,3 +5,4 @@ version := "1.0"
 scalaVersion := "2.12.15"
 
 libraryDependencies ++= Seq("org.scalatest" %% "scalatest" % "3.2.9" % Test)
+
--- src/main/scala/org/craftedsw/tripservicekata/trip/TripService.scala
+++ src/main/scala/org/craftedsw/tripservicekata/trip/TripService.scala
@@ -3,21 +3,28 @@ package org.craftedsw.tripservicekata.trip
 import org.craftedsw.tripservicekata.exception.UserNotLoggedInException
 import org.craftedsw.tripservicekata.user.{User, UserSession}
 
-class TripService(getLoggedUser: () => Option[User], findTripsByUser: User => List[Trip] ) {
+class TripService(getLoggedUser: () => Option[User],
+                  findTripsByUser: User => List[Trip]) {
 
-	def this(getLoggedUser: () => User) = this(() => Option(getLoggedUser()), _ => Nil)
+  def this(getLoggedUser: () => User) = this(
+    () => Option(getLoggedUser()),
+    TripDAO.findTripsByUser
+  )
 
-	def this() =  this( () => Option(UserSession.getLoggedUser), TripDAO.findTripsByUser )
+  def this() = this(
+    () => Option(UserSession.getLoggedUser),
+    TripDAO.findTripsByUser
+  )
 
-	def getTripsByUser(user: User): List[Trip] = {
+  def getTripsByUser(user: User): List[Trip] = {
     getLoggedUser().fold(
-			ifEmpty = throw new UserNotLoggedInException
-		){ loggedInUser =>
+      ifEmpty = throw new UserNotLoggedInException
+    ) { loggedInUser =>
       user.isFriend(loggedInUser) match {
-				case true => findTripsByUser(user)
-				case false => List.empty[Trip]
-			}
-		}
-	}
+        case true => findTripsByUser(user)
+        case false => List.empty[Trip]
+      }
+    }
+  }
 
 }
